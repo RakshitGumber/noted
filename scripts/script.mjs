3b9fc8832bbd11774991332afee0143aaaaa1425
RakshitGumber <gumberrakshit@gmail.com>
2025-08-10 21:32:44 +0530

    Made Header

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,9 +1,12 @@
 const themeToggle = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
 
 themeToggle.addEventListener("click", () => {
   if (document.body.classList.contains("dark")) {
     document.body.classList.remove("dark");
+    themeIcon.setAttribute("icon", "carbon:sun");
   } else {
     document.body.classList.add("dark");
+    themeIcon.setAttribute("icon", "carbon:moon");
   }
 });
