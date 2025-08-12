cfc1c0128612b4e64053b0dfeb065d7862ac0309
RakshitGumber <gumberrakshit@gmail.com>
2025-08-12 15:29:57 +0530

    Completed the notes app

--- src/scripts/script.mjs
+++ src/scripts/script.mjs
@@ -1,5 +1,8 @@
 const themeToggle = document.getElementById("theme-toggle");
 const themeIcon = document.getElementById("theme-icon");
+const notesSection = document.querySelector(".notes");
+
+render();
 
 themeToggle.addEventListener("click", () => {
   if (document.body.classList.contains("dark")) {
@@ -15,5 +18,30 @@ const noteForm = document.querySelector(".note-form");
 
 noteForm.children.namedItem("submit-button").addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(noteForm.children.namedItem("notes").value);
+  const data = noteForm.children.namedItem("notes").value;
+  let notes = [];
+  if (localStorage.getItem("notes"))
+    notes = JSON.parse(localStorage.getItem("notes"));
+
+  notes.push(data);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  render();
 });
+
+function render() {
+  if (localStorage.getItem("notes")) {
+    notesSection.innerHTML = JSON.parse(localStorage.getItem("notes"))
+      .map((note, index) => {
+        return `<article class="note-item" id="${index}">
+      <span>
+      ${note}
+      </span>
+      </article>`;
+      })
+      .join(" ");
+  } else {
+    notesSection.innerHTML = `<div class="no-item-placeholder">No Items Found</div>`;
+  }
+}
